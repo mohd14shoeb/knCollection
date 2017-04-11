dabc619a6e12a0b047de25e023f6712799ea1516
Ky Nguyen <kynguyen@KNs-MBP.local>
2017-04-12 05:24:46 +0700

    Remove some duplicated functions. add demo photo for AnimateButton

--- knCollection/Extension/knUIImage.swift
+++ knCollection/Extension/knUIImage.swift
@@ -9,22 +9,13 @@
 import UIKit
 extension UIImageView {
     
-    func blurBackground() {
+    func blur() {
         
         let darkBlur = UIBlurEffect(style: UIBlurEffectStyle.dark)
         let blurView = UIVisualEffectView(effect: darkBlur)
         blurView.frame = frame
         blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         addSubview(blurView)
-    }
-    
-    static func makeImage(with color: UIColor, size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(CGRect(origin: CGPoint.zero, size: size))
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
     }
     
     func change(color: UIColor) {
